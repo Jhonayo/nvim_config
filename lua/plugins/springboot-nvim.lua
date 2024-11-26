45cbf7eeb3f430378107e25839b4af1de3471bbd
jhonayo <j.iramirezpavez@gmail.com>
2024-11-26 03:27:56 -0300

    incorporacion de plugin springboot / ajuste de colores lualine v1.3

---
+++ lua/plugins/springboot-nvim.lua
@@ -0,0 +1,21 @@
+return {
+  "elmcgill/springboot-nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "msfussenegger/nvim-jdtls",
+    "nvim-tree/nvim-tree.lua",
+  },
+  config = function()
+    --gain access to the springboot nvim pluggin and its funtions
+    local springboot_nvim = require("springboot-nvim")
+
+    --set a vim motion to <Space> + <Shift> + r to run the spring boot project
+    vim.keymap.set('n', '<leader>jh', springboot_nvim.boot_run, { desc = "[J]ava [R]un Springboot" })
+    vim.keymap.set('n', '<leader>jcc', springboot_nvim.generate_class, { desc = "[J]ava Create [C]lass" })
+    vim.keymap.set('n', '<leader>jci', springboot_nvim.boot_run, { desc = "[J]ava Create [I]nterface" })
+    vim.keymap.set('n', '<leader>jce', springboot_nvim.boot_run, { desc = "[J]ava Create [Enum]" })
+
+    springboot_nvim.setup({})
+
+  end
+}
